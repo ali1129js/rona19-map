4327a21115a99a98c475b4c9a54df806ae5434d1
ali1129js <ali1129js@gmail.com>
2020-05-05 22:56:47 +0200

    clean

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/**
+ * @Author: Ali
+ * @Date:   2020-05-05T22:54:53+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2020-05-05T22:55:41+02:00
+ */
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <map name=""></map>
+    </div>
+  );
+};
+
+export default App;
