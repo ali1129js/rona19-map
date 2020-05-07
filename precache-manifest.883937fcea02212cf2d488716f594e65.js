86d03c61b07e154befd754d9623c14fef6f701ee
ali1129js <ali1129js@gmail.com>
2020-05-07 17:43:54 +0200

    Updates

---
+++ precache-manifest.883937fcea02212cf2d488716f594e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1074f0a55aa7c20f38af064bbaa5e5",
+    "url": "/rona19-map/index.html"
+  },
+  {
+    "revision": "f0597da3abf8b657752f",
+    "url": "/rona19-map/static/css/main.0f200195.chunk.css"
+  },
+  {
+    "revision": "038b61027cfa203f2d14",
+    "url": "/rona19-map/static/js/2.2dd01bf7.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/rona19-map/static/js/2.2dd01bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0597da3abf8b657752f",
+    "url": "/rona19-map/static/js/main.b47f080d.chunk.js"
+  },
+  {
+    "revision": "13e838d801b0484ab017",
+    "url": "/rona19-map/static/js/runtime-main.3c353b89.js"
+  }
+]);
